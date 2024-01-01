67a5439291b79a82766f809b3e1ab21ad5ef07fc
Emito Khoza <emito.khoza@gmail.com>
2024-01-01 11:22:22 +0200

    Starter files

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Header from './Header.jsx';
+import Footer from './Footer.jsx';
+import Food from './Food.jsx';
+
+function App() {
+  return (
+    <>
+      <Food />
+      <Footer />
+      <Header />
+      <Food />
+    </>
+  ); 
+}
+
+export default App;
